4981b0cf193abe0c7912d793d995633c611a6b02
deltonmyalil <deltonguns@gmail.com>
2018-06-16 21:15:11 +0530

    Linear probing Implementation using array - incomplete

---
+++ src/part_2_data_structures/lesson_2_linear_probing_implementation/Employee.java
@@ -0,0 +1,42 @@
+package part_2_data_structures.lesson_2_linear_probing_implementation;
+
+public class Employee {
+	private String firstName;
+	private String lastName;
+	private int id;
+
+	public Employee(String firstName, String lastName, int id) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() { //To make printHashTable to print names
+		return this.firstName + " " + this.lastName;
+	}
+}
