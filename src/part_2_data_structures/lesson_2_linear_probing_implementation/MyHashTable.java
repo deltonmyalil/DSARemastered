b8fd468e8c75aa2e6d2e56141fb9299c15ffaa7e
deltonmyalil <deltonguns@gmail.com>
2018-06-17 21:11:28 +0530

    Linear probing Implementation using array - get() and remove() methods implemented

--- src/part_2_data_structures/lesson_2_linear_probing_implementation/MyHashTable.java
+++ src/part_2_data_structures/lesson_2_linear_probing_implementation/MyHashTable.java
@@ -1,10 +1,10 @@
 package part_2_data_structures.lesson_2_linear_probing_implementation;
 
 public class MyHashTable {
-	private Employee[] hashtable;
+	private StoredEmployee[] hashtable;
 
 	public MyHashTable() {
-		hashtable = new Employee[10];
+		hashtable = new StoredEmployee[10];
 	}
 
 	private int hashKey(String key) {
@@ -13,34 +13,66 @@ public class MyHashTable {
 
 	public void put(String key, Employee employee) {
 		int hashedKey = hashKey(key);
-		if(occupied(hashedKey)) {
+		if (occupied(hashedKey)) {
 			int stopIndex = hashedKey; //already looked
-			if(hashedKey == hashtable.length-1)
-				hashedKey=0;
+			if (hashedKey == hashtable.length - 1)
+				hashedKey = 0;
 			else
 				hashedKey++;
 			//the above 4 lines set the first probe position
 
-			while(occupied(hashedKey) && hashedKey!=stopIndex) {
-				hashedKey = (hashedKey+1)%hashtable.length;
+			while (occupied(hashedKey) && hashedKey != stopIndex) {
+				hashedKey = (hashedKey + 1) % hashtable.length;
 			}
 			//the above 2 lines set the next probe positions
 		}
 
 		if (occupied(hashedKey)) System.out.println("HashTable is full. Linear Probing failed");
-		else hashtable[hashedKey] = employee;
+		else hashtable[hashedKey] = new StoredEmployee(key, employee);
 	}
 
-	public Employee get(String key){
-		return hashtable[hashKey(key)];
+	public Employee get(String key) {
+		int hashedKey = findKey(key);
+		if (hashedKey == -1) return null;
+		else return hashtable[hashedKey].employee;
+	}
+
+	private int findKey(String key) {
+		int hashedKey = hashKey(key);
+		if (hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key))
+			return hashedKey;
+
+		int stopIndex = hashedKey;
+		if (hashedKey == hashtable.length - 1)
+			hashedKey = 0;
+		else hashedKey++;
+
+		while (hashedKey != stopIndex && hashtable[hashedKey] != null && !hashtable[hashedKey].key.equals(key))
+			hashedKey = (hashedKey + 1) % hashtable.length;
+
+		if(hashtable[hashedKey] != null && hashtable[hashedKey].key.equals(key)) return hashedKey;
+		else return -1;
 	}
 
 	public void printHashTable() {
-		for(int i = 0; i<hashtable.length;i++) System.out.println(hashtable[i]);
+		for (int i = 0; i < hashtable.length; i++) {
+			if (hashtable[i] == null) System.out.println("empty");
+			else System.out.println(hashtable[i].employee);
+		}
 	}
 
 	private boolean occupied(int index) {
 		if (hashtable[index] != null) return true; //it is occupied
 		else return false; //it is free
+	}
+
+	public Employee remove(String key) {
+		int hashedKey = findKey(key);
+
+		if(hashedKey == -1) return null;
+
+		Employee employee = hashtable[hashedKey].employee;
+		hashtable[hashedKey] = null;
+		return employee;
 	}
 }
